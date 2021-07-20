c6368d2d054645114e32c81bf7fc0a48094625db
Eric Downes <eric@triskew.com>
2021-07-20 00:01:29 -0400

    base type now tuple; finished closure fcn

--- row_compose.py
+++ row_compose.py
@@ -1,15 +1,14 @@
 
 
 
-class Endo(list):
-    def __init__(self, l:List[int]):
-        super().__init__([i for i in l if isinstance(i, int)])
-        self.is_id = self == list(range(len(self)))
+class Endo(tuple):
+    
+    def __init__(self, it:Iterable[int]):
+        n = len(it)
+        super().__init__(i % n for i in it)
+        self.is_id = self == tuple(range(len(self)))
         
-    def __mul__(self, other:Union[int, List[int]]) -> Endo:
-        return self(other)
-        
-    def __call__(self, other:Union[int, List[int]], strict:bool = True) -> Endo:
+    def __call__(self, other:Union[int, Iterable[int]], strict:bool = True) -> Endo:
         if isinstance(other, int):
             return self[other % len(self)]
         if strict:
@@ -18,11 +17,23 @@ class Endo(list):
             return other
         if isinstance(other, Endo) and other.is_id:
             return self
-        return [self[i % len(self)] for i in other]
+        return Endo(tuple(self[i % len(self)] for i in other))
         
-    def __rmul__(self, other:List[int]) -> Endo:
+    def __mul__(self, other:Union[int, Iterable[int]]) -> Endo:
+        return self(other)
+        
+    def __rmul__(self, other:Iterable[int]) -> Endo:
         return Endo(other)(self)
 
+    
+def closure(endos:List[Endo]) -> List[Endo]:
+    trmag = {e:str(i) for i, e in enumerate(endos)}
+    for i, u in enumerate(endos):
+        for j, v in enumerate(endos):
+            if (x := u(v)) not in trmag:
+                trmag.update({x:f'{i}.{j}'})
+    return len(trmag) == len(endos), trmag
+    
 class Coset(set):
     def __init__(self, *args, op:Binop = None, **kwargs):
         super().__init__(*args, **kwargs)
