eafcb2d6dc6909e03c6b2c88b4551850119dd58a
Eric Downes <eric@triskew.com>
2022-12-25 03:44:30 -0800

    thing

--- dec22.py
+++ dec22.py
@@ -13,41 +13,29 @@ from monoids import cosets, row_monoid
 from demo import octos
 
 OCTS = set(range(16))
+G = row_monoid(octos).monoid_table
+
 def too_many_octs(r) -> bool:
     return len(OCTS.intersection(r)) > 1
 
-def searcher(name:str,
-             shape:tuple[int,int],
-             dtype:type,
-             combo:tuple[int,...]) -> None:
-    with SharedMemoryManager() as smm:
-        shm = smm.SharedMemory(name = name)
-        G = ndarray(shape = shape, dtype = dtype, buffer = shm.buf)
-        M, melem = submagma(G, combo, max_order = 8)
-        if len(melem) != 8 or \
-           too_many_octs(melem) or \
-           is_abelian(M) and is_unital(M) or \
-           not cosets(G, melem, bail_on = too_many_octs):
+def searcher(combo:tuple[int,...]) -> None:
+    M, melem = submagma(G, combo, max_order = 8)
+    if len(melem) != 8 or \
+       too_many_octs(melem) or \
+       is_abelian(M) and is_unital(M) or \
+       not cosets(G, melem, bail_on = too_many_octs):
             return
     print(now(), 'SUCCESS Found a candidate divisor!!!', melem)
     sig = '_'.join(melem)
     DataFrame(M).to_csv(f'candidates/{sig}.csv', header=False, index=False)
 
-def combos(a:NDArray, upper_bound:int) -> Iterator[tuple[int,...]]:
-    for n in range(1, upper_bound):
-        for combo in combinations(range(len(a)), n):
+def combos(n:int, u:int) -> Iterator[tuple[int,...]]:
+    for m in range(1, u):
+        for combo in combinations(range(n), m):
             if len(OCTS.intersection(combo)) <= 1:
                 yield combo
         print(now(), f'queued all at rank={n}')
 
-def main():
-    xs128 = row_monoid(octos).monoid_table
-    with SharedMemoryManager() as smm, Pool() as pool:
-        shm = smm.SharedMemory(size = xs128.nbytes)
-        shared = ndarray(shape = xs128.shape, dtype = xs128.dtype, buffer = shm.buf)
-        shared[:] = xs128[:]
-        fcn = partial(searcher, name = shm.name, dtype = xs128.dtype, shape = xs128.shape)
-        pool.map(fcn, combos(xs128, 7))
-
 if __name__ == '__main__':
-    main()
+    with Pool() as pool:
+        pool.map(searcher, combos(len(G), 7))
